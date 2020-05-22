678ac6454bec18913d1605320e67a9f4c5cf02f0
lolkobr4 <58613298+lolkobr4@users.noreply.github.com>
2020-05-22 19:39:14 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Taxi system with chat based functions. Connected with a Database, designed for u
 			ID			=		0		
 			requestName =		1
 			requestLocation	=	2		// returns X,Y,Z
-			accepted		=	3		// returns "True"/"False"
+			accepted		=	3		// returns 0/1
 		
 		taxiDriver:
 			id				=	0	
@@ -29,24 +29,29 @@ Taxi system with chat based functions. Connected with a Database, designed for u
 
 # Code based:
 	disco (https://www.youtube.com/watch?v=G_H0rJj1gJc)
-	people need to "apply"										- DONE
+	people need to "apply"										- DONE + TESTED
 	onplayerconnect get taxidriver permission with DB check		- DONE
 	Taxi request list											- DONE
 	Basic help cmd												- PARTIAL
-	
-	order a taxi
+	order a taxi												- DONE
+	message requester if taxi is  coming						- DONE
 	cancel an order
-	delete an order after X secs
+	delete an order after X secs								- DONE
 	end an order when job is done.
 
-	Accept as driver + createMarker
+	Accept as driver + createMarker								- PARTIAL
+	cant create a marker. Doesnt even work with player.position
 
 	discord whook
-	setup taxiDriver Permission
+	setup taxiDriver Permission (see above)
 	driver <-> customer // private chat (could be unnecessary)
+
+	create a variable to delete order after X seconds instead
+	of hardcoding it
 
 # Other
 	Create Example Permission Setup for TaxiDriver in XML
+
 # Chat / Notes
 
 	---------------------- Notes box ----------------------
--- taxiscript.cpp
+++ taxiscript.cpp
@@ -1,78 +1,3 @@
-taxicall = array();
-
-command taxi(rsn) {
-	permission = "taxi.call";
-	execute() {
-		if (taxicall.contains(player.id) == false) {
-			reason = "None";
-			if (isSet(rsn)) { reason = str.join(" ", arguments); }
-
-			foreach(taxi in server.players) {
-				if (taxi.hasPermission("taxi")) { taxi.message(player.name + " has called a taxi. Destination: " + reason + ". Accept this job? /accept"); }
-			}
-			taxicall.add(player.id);
-			player.message("You have already called a taxi!");
-			wait.seconds(60, waitRemove(player.id));
-		}
-		else { player.message("You have already called a taxi!"); }
-	}
-}
-
-command respond() {
-	permission = "taxi";
-	execute() {
-		if (player.hasPermission("taxi")) {
-			if (taxicall[0] != null) {
-				targetPlayer = toPlayer(taxicall[0]);
-				if (isPlayer(targetPlayer)) {
-					targetName = targetPlayer.name;
-					location = targetPlayer.position;
-					player.setMarker(location);
-					taxicall.remove(targetPlayer.id);
- 
-					foreach(taxi in server.players) {
-						if (taxi.hasPermission("taxi")) { taxi.message(player.name + " has accepted " + targetName + " call!"); }
-					}
-
-					player.message("Placed a marker on " + targetName + " location!");
-					targetPlayer.message(player.name + " has accepted your request, they are making their way to you now!");
-				}
-				else { player.message("No calls to answer!"); }
-			}
-			else { player.message("No calls to answer!"); }
-		}
-		else { player.message("You aren't a taxi driver!"); }
-	}
-}
-
-command responds() {
-	permission = "taxi";
-	execute() {
-		responds = toInt(taxicall.count);
-		player.message("Pending jobs: " + responds);
-	}
-}
-
-command taxihelp() {
-	permission = "taxi";
-	execute() {
-		player.message("Taxi Commands");
-		player.message("/taxihelp = Shows all the commands related to Taxis.");
-		player.message("/respond = Allows drivers to accept calls. First come first serve.");
-		player.message("/responds = Displays all pending jobs.");
-	}
-}
-
-function waitRemove(myPlayer) {
-	playerRemove = toPlayer(myPlayer);
-	if (isPlayer(playerRemove) {
-		if (taxicall.contains(playerRemove.id) {
-			taxicall.remove(playerRemove.id);
-				playerRemove.message("No taxi drivers accepted your job. Feel free to send another now.");
-		}
-	}
-}
-
 /////////////////////////////////////////////////////////////////////////////////////////
 // EVERYTHING RELATED TO TAXI ORDERS
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -83,20 +8,25 @@ command acceptOrder(requestID) {
 
 			if (Result.count != 0) {
 				ResultArray = Result[0];
+				requestID = ResultArray[0];
 				requestName = ResultArray[1];
 				requestLocation = ResultArray[2];
 				requestaccepted = ResultArray[3];
 
-				if (requestaccepted == "False") {
+				if (requestaccepted == 0) {
 					logger.log("acceptOrder - requestaccepted returned 'False'");
+					database.execute("UPDATE taxiRequest SET accepted='1' WHERE id = '" + requestID + "';");
+					player.message(str.format("<color=cyan>You have accepted {0}'s order!</color>", requestName));
 				}
 				else {
 					logger.log("acceptOrder - requestaccepted returned something different than 'False'");
+					player.message(str.format("<color=red>Order #{0} has already been accepted by someone else!</color>", requestID));
 				}
 			}
 		}
 	}
 }
+
 command orderTaxi() {
 	//permission = "taxi.call";
 	execute() {
@@ -107,20 +37,31 @@ command orderTaxi() {
 		positionY = player.position.y;
 		positionZ = player.position.z;
 		saveToDBPos = toString(positionX) + "," + toString(positionY) + "," + toString(positionZ);
-		Result = database.execute("SELECT * FROM taxiRequest WHERE steamID64 = '" + playerID + "';");
+		Result = database.execute("SELECT * FROM taxiRequest WHERE requestName = '" + playerName + "';");
 
 		if (Result.count != 0) {
-			player.message(str.format("<color=red>You have already called a Taxi!</color>"));
-		}
-		else {
-			player.message(str.format("<color=red>You have ordered a Taxi!</color>"));
-			database.execute("INSERT INTO taxiRequest (requestName, requestLocation) VALUES ('" + playerName + "', '" + saveToDBPos + "');");
-		}
+			Result = database.execute("SELECT * FROM taxiRequest WHERE requestName = '" + playerName + "' AND accepted = 0;");
+			if (Result.count != 0) {
+				// Cant make a new order
+				player.message(str.format("<color=red>You have already called a Taxi!</color>"));
+			}
+			else {
+				//can make a new order.
+				ResultArray = Result[0];
+				requestID = ResultArray[0];
+				requestName = ResultArray[1];
+				requestLocation = ResultArray[2];
+				requestaccepted = ResultArray[3];
 
-		foreach(x in server.players) {
-			//if (taxi.hasPermission("taxiDriver")) { taxi.message(player.name + " has called a taxi. Destination: " + reason + ". Accept this job? /accept"); }
-			if (x.hasPermission("taxi.job")) { //change that to permissions once created.
-				taxiDriver.message(str.format("<color=yellow>A new order has been made!</color>", player.name));
+				foreach(x in server.players) {
+					//if (taxi.hasPermission("taxiDriver")) { taxi.message(player.name + " has called a taxi. Destination: " + reason + ". Accept this job? /accept"); }
+					if (x.hasPermission("taxi.job")) { //change that to permissions once created.
+						x.message(str.format("<color=yellow>A new order has been made!</color>", player.name));
+					}
+				}
+
+				player.message(str.format("<color=yellow>You have ordered a Taxi!</color>"));
+				database.execute("INSERT INTO taxiRequest (requestName, requestLocation) VALUES ('" + playerName + "', '" + saveToDBPos + "');");
 			}
 		}
 	}
@@ -135,9 +76,9 @@ command orderTaxi() {
 command taxiHelp() {
 	//permission = "taxi.help";
 	execute() {
-		player.message(str.format("<color=cyan>/joinTaxi to get a job as Taxi Driver</color>", requestID, requestName));
-		player.message(str.format("<color=cyan>/quitTaxi to quit the job as Taxi Driver.</color>", requestID, requestName));
-		player.message(str.format("<color=cyan>/taxirequests lists all Taxi orders</color>", requestID, requestName));
+		player.message(str.format("<color=cyan>/joinTaxi to get a job as Taxi Driver</color>"));
+		player.message(str.format("<color=cyan>/quitTaxi to quit the job as Taxi Driver.</color>"));
+		player.message(str.format("<color=cyan>/taxirequests lists all Taxi orders</color>"));
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -182,12 +123,13 @@ command joinTaxi() {
 		Result = database.execute("SELECT * FROM taxiDriver WHERE steamID64 = '" + playerID + "';");
 
 		if (Result.count != 0) {
+			player.message(str.format("<color=yellow>You're already a Taxi Driver.</color>"));
+
+		}
+		else {
 			player.message(str.format("<color=yellow>Would you like to apply as a taxi driver?</color>"));
 			player.message(str.format("<color=yellow>Please confirm with /applyConfirm</color>"));
 			player.setData("joinTaxi", true);
-		}
-		else {
-			player.message(str.format("<color=yellow>You're already a Taxi Driver.</color>"));
 		}
 	}
 }
@@ -195,17 +137,17 @@ command joinTaxi() {
 command applyconfirm() {
 	permission = "taxi.job";
 	execute() {
-		if (player.getData("joinTaxi")) {
+		if (player.getData("joinTaxi") and player.getData("joinTaxi") != null) {
 
 			playerID = player.id;
 			playerName = player.name;
 
 			database.execute("INSERT INTO taxiDriver (steamID64, taxiDriverName) VALUES ('" + playerID + "', '" + playerName + "');");
 			player.message(str.format("<color=yellow>You are now a Taxi Driver!</color>"));
-
+			player.setData("joinTaxi", false);
 		}
 		else {
-			player.message(str.format("<color=yellow>Please use /joinTaxi to apply to be a taxi driver</color>"));
+			//player.message(str.format("<color=yellow>Please use /joinTaxi to apply to be a taxi driver</color>"));
 		}
 	}
 }
@@ -231,30 +173,32 @@ command quitTaxi() {
 command quitconfirm() {
 	permission = "taxi.job";
 	execute() {
-		if (player.getData("quitTaxi")) {
+		if (player.getData("quitTaxi") and player.getData("quitTaxi") != null) {
+			playerID = player.id;
 			player.message(str.format("<color=yellow>You have quit your job as Taxi Driver.</color>"));
 			database.execute("DELETE FROM taxiDriver WHERE steamID64 = '" + playerID + "';");
+			player.setData("quitTaxi", false);
 		}
 		else {
 			// we dont need a warning if they want to quit a job that they are not having lol.
 			// could be a nice easter egg, though.
-			player.message(str.format("<color=yellow>Error: Cant quit a job you dont have!11!! The Devs are watching you.. </color>"));
+			player.message(str.format("<color=yellow>Error: Cant quit a job you dont have!11!!</color><color=red> The Devs are watching you.. </color>"));
 		}
 	}
 }
 
 event onPlayerJoined(player) {
-	playerID = player.ID;
+	playerID = player.id;
 	Result = database.execute("SELECT * FROM taxiDriver WHERE steamID64 = '" + playerID + "';");
 
 	if (Result.count != 0) {
 		player.addGroup("TaxiDriver");
-		player.message(str.format("<color=yellow>Welcome back! You are back on your job as TaxiDriver."));
+		player.message(str.format("<color=yellow>Welcome back! You are back on your job as TaxiDriver.</color>"));
 	}
 }
 
 event onPlayerQuit(player) {
-	playerID = player.ID;
+	playerID = player.id;
 	Result = database.execute("SELECT * FROM taxiDriver WHERE steamID64 = '" + playerID + "';");
 
 	if (Result.count != 0) {
